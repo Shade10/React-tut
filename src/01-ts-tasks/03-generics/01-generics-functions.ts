2f0f52d087fd8db715f085996523705fc41cf22a
Piotr Woloszun <piotr.woloszun@gmail.com>
2018-05-27 22:29:38 +0200

    v1.0.2

--- src/01-ts-tasks/03-generics/01-generics-functions.ts
+++ src/01-ts-tasks/03-generics/01-generics-functions.ts
@@ -1,13 +1,14 @@
-// TODO 1: MapIterator interface; gMap function
 
-// TODO 2: FilterIterator interface; gFilter function
+// TODO 1: MappingFunction interface; gMap function
 
-// TODO 3: AllIterator interface; gAll function
+// TODO 2: FilteringFunction interface; gFilter function
 
-// TODO 4: SliceIterator interface; gEachSlice function
+// TODO 3: AllPredicateFunction interface; gAll function
 
-// TODO 5: ReduceIterator interface; gReduce function
+// TODO 4: SlicePredicateFunction interface; gEachSlice function
+
+// TODO 5: AccumulatorFunction interface; gReduce function
 
 // TODO 6: gPluck function
 
-// TODO 7: interfaces: GroupByIterator & Grouped; gGroupBy function
+// TODO 7: interfaces: GroupByPredicateFunction & Grouped; gGroupBy function
