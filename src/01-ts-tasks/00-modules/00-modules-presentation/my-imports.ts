d19c412500a1b187363b12b1fa9b2fdfdd0fafa2
Piotr Woloszun <piotr.woloszun@gmail.com>
2018-05-28 13:18:13 +0200

    v1.1.0

--- src/01-ts-tasks/00-modules/00-modules-presentation/my-imports.ts
+++ src/01-ts-tasks/00-modules/00-modules-presentation/my-imports.ts
@@ -1,5 +1,4 @@
 import { xxx, ggg } from './some-subdir/my-exports';
-import myExports from './some-subdir/my-exports';
 
 export function myImportsApp() {
   console.log('myImportsApp', xxx, ggg());
