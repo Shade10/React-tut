d0a74c949d988a75c1763f20c506be66a9a4198d
Piotr Woloszun <piotr.woloszun@gmail.com>
2019-11-18 01:31:36 +0100

    v1.2.1 +libs bump up

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.ts',
@@ -21,7 +21,7 @@ module.exports = {
     contentBase: './dist'
   },
   plugins: [
-    new CleanWebpackPlugin(['dist']),
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: 'src/assets/template.html'
     })
